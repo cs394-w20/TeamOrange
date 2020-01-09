0671f9a8ccb7523d129633d23062445f42ca5f04
Utkarsh Mishra <utkarshmishra2022@u.northwestern.edu>
2020-01-09 12:15:47 -0600

    first commit Utkarsh

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ function App() {
       <Menu attached="top">
         <Menu.Item>Home</Menu.Item>
       </Menu>
+      <Menu attached="top">
+        <Menu.Item>Workout</Menu.Item>
+      </Menu>
+
     </Container>
   );
 }
