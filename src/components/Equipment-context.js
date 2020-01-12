8b050f4a1ddb852cbac7c97b05b7851ede90811f
Ka Wong <kawong2020@u.northwestern.edu>
2020-01-12 13:14:42 -0600

    add context api to share equipment list

--- src/components/Equipment-context.js
+++ src/components/Equipment-context.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 export const EquipmentContext = React.createContext({
-    lst:[],
-    updatelst:({new-lst}) => 
+    lst: [],
+    updateLst: () => {}
 })
--- src/input.js
+++ src/input.js
@@ -1,28 +1,36 @@
-import React, { useEffect, useState } from 'react';
-import { Container, Menu, Checkbox } from 'semantic-ui-react';
+import React, { useEffect, useState } from "react";
+import { Container, Menu, Checkbox } from "semantic-ui-react";
+import { EquipmentContext } from "./components/Equipment-context";
 
 const EquipmentList = ({ items }) => {
-    const [lst, setLst] = useState([])
-
-    return (
+  return (
+    <EquipmentContext.Consumer>
+      {({ lst, updateLst }) => (
         <div>
-            {items.map(item => <Equipment state={{ lst, setLst }} item={item} />)}
+          {items.map(item => (
+            <Equipment key={item.id} state={{ lst, updateLst }} item={item} />
+          ))}
         </div>
-        <button>
-            Okay
-        </button>
-    )
+      )}
+    </EquipmentContext.Consumer>
+  );
 };
 
 const Equipment = ({ item, state }) => {
-
-    return (<div>
-        <Checkbox onClick={() => {
-            state.setLst(state.lst.includes(item.title) ? state.lst.filter(y => y !== item.title) : [item.title].concat(state.lst));
-            console.log(state.lst);
-        }} />
-        <span> {item.title}</span>
-    </div>);
+  return (
+    <div>
+      <Checkbox
+        onClick={() => {
+          state.updateLst(
+            state.lst.includes(item.title)
+              ? state.lst.filter(y => y !== item.title)
+              : [item.title].concat(state.lst)
+          );
+        }}
+      />
+      <span> {item.title}</span>
+    </div>
+  );
 };
 
-export def
+export default EquipmentList;
