96bb0f4248c6e965da48c172ba50771cae2f5ab8
Ka Wong <kawong2020@u.northwestern.edu>
2020-01-11 22:49:36 -0600

    add exercises to exercises-alt.json + 10 equipments

--- src/App.js
+++ src/App.js
@@ -24,23 +24,43 @@ const welcome = {
   ,equipment_list: [
     {
       "id": "1",
-      "title": "Dumbbells",
+      "title": "Jumping rope",
     },
     {
       "id": "2",
-      "title": "Barbells",
+      "title": "Power ball",
     },
     {
       "id": "3",
-      "title": "Pullup-Up Bar",
+      "title": "Dumbbells",
     },
     {
       "id": "4",
-      "title": "Dip Bar",
+      "title": "Barbell / Plates",
     },
     {
       "id": "5",
+      "title": "Pull up bar",
+    },
+    {
+      "id": "6",
       "title": "Kettlebell",
+    },
+    {
+      "id": "7",
+      "title": "Jumping box",
+    },
+    {
+      "id": "8",
+      "title": "Rings",
+    },
+    {
+      "id": "9",
+      "title": "Resistance Band",
+    },
+    {
+      "id": "10",
+      "title": "Sandbag",
     }
   ]
 };
