c396e337c6981f8e7e455610721b055d0608a05c
Utkarsh Mishra <utkarshmishra2022@u.northwestern.edu>
2020-01-22 18:23:58 -0600

    Update Workout.js

--- workout-generator/src/Components/Workout.js
+++ workout-generator/src/Components/Workout.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { Card, Embed, Button } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
-const Workout = ({ exercise, setCountdown, toggleFavs, favworkouts }) => (
+const Workout = ({ exercise, setCountdown, toggleFavs, favworkouts }) =>{ 
+  return(
   <Card color="blue">
     <Card.Content>
       <Card.Header>{exercise.Title}</Card.Header>
@@ -14,25 +15,31 @@ const Workout = ({ exercise, setCountdown, toggleFavs, favworkouts }) => (
       </Card.Content>
     </Card.Content>
     <Card.Content extra>
-      <Button 
+      <Button
         as={Link}
         to="/timer"
+        fluid
         onClick={setCountdown}
-        color='green' 
+        color='green'
         icon='time'
         content="TIMER"
-        attached="top"
       />
-      <Button 
+      <Button
+        color='blue'
+        fluid
+        icon='forward'
+        content="REPLACE"
+      />
+      <Button
+        fluid
         icon={favworkouts.includes(exercise) ? "minus" : "plus"}
-        basic 
-        color='blue' 
+        basic
+        color='blue'
         content={favworkouts.includes(exercise) ? "REMOVE FROM FAVORITES" : "SAVE TO FAVORITES"}
-        attached="bottom"
-        onClick = {() => toggleFavs(exercise)}
+        onClick={() => toggleFavs(exercise)}
       />
     </Card.Content>
   </Card>
-);
+)};
 
 export default Workout
