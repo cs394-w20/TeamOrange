79dec0047d492de8c95056bdc772e86f9c70d0a7
Ka Wong <kawong2020@u.northwestern.edu>
2020-02-07 18:40:59 -0600

    add modal window based on cookie info

--- workout-generator/src/Pages/SplashPage.js
+++ workout-generator/src/Pages/SplashPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Grid } from 'semantic-ui-react';
 import EquipmentForm from '../Components/EquipmentForm';
+import ModalWindow from '../Components/ModalWindow';
 
 const SplashPage = () => {
   return (
     <Grid centered style={{ marginTop: "15px" }}>
       <Grid.Row>
+        <ModalWindow />
         <EquipmentForm />
       </Grid.Row>
     </Grid>
