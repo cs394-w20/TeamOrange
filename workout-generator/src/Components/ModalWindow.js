0e0202b1eed3b4c04b1fb52391770b6c5d738407
omofish <jason_lzx@hotmail.com>
2020-02-10 16:07:55 -0600

    changed modal

--- workout-generator/src/Components/ModalWindow.js
+++ workout-generator/src/Components/ModalWindow.js
@@ -20,26 +20,28 @@ const ModalWindow = () => {
   console.log("cookies:", localStorage);
 
   return (
-    <Modal size="small" defaultOpen={showInfo} closeIcon>
-      <Modal.Header>What is H.I.I.T.?</Modal.Header>
+    <Modal size="medium" defaultOpen={showInfo} closeIcon>
+      <Modal.Header>What is NU HIIT?</Modal.Header>
       <Modal.Content image>
         <Image
           wrapped
-          size="medium"
+          size="large"
           src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/701/one-change-improve-hiit-workout-preformance-1513345031.jpg?resize=480:*"
         />
         <Modal.Description>
-          <Header>Here's what we found</Header>
           <p>
-            Cardiovascular exercise strategy alternating short periods of
-            intense anaerobic exercise with less intense recovery periods.
-            Sounds fun right?
+            High-Intensity Interval Training (HIIT) is a cardiovascular exercise strategy alternating
+            short periods of intense anaerobic exercise with less intense recovery periods.
+            It is an effective way to lose weight and stay healthy.
+          </p>
+          <p>
+            NU HIIT helps generate the ideal HIIT workout based on your equipment and individual needs!
           </p>
         </Modal.Description>
       </Modal.Content>
       <Checkbox
         style={{ margin: "20px", marginTop: "0px" }}
-        label="Do not show on my next visit."
+        label="Do not show this message again."
         onChange={onChange}
       />
     </Modal>
