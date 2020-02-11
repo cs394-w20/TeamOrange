0ae10a620be24c030e4fd00727db2b216980c486
Ka Wong <kawong2020@u.northwestern.edu>
2020-02-10 19:31:35 -0600

    quick adjustments to equip imgs

--- workout-generator/src/constants.js
+++ workout-generator/src/constants.js
@@ -3,67 +3,67 @@ export const EQUIPMENT_LIST = [
     id: "1",
     title: "Jump Rope",
     description: "a rope!",
-    url: "https://images-na.ssl-images-amazon.com/images/I/71aRUlvJegL._AC_SL1500_.jpg"
+    url: "./assets/rope.jpg"
   },
   {
     id: "2",
     title: "Power Ball",
     description: "a ball!",
-    url: "https://image.made-in-china.com/43f34j00amWQTVwCCogj/Power-Training-Fitness-Iron-Sand-Inside-Gym-Ball-Dead-Ball-Slam-Ball.jpg"
+    url: "./assets/ball.jpg"
   },
   {
     id: "3",
     title: "Dumbbells",
     description: "a dumbbell!",
-    url: "https://yorkbarbell.com/wp-content/uploads/2018/06/25-2.jpg"
+    url: "./assets/dumbbell.jpg"
   },
   {
     id: "4",
     title: "Barbell / Plates",
     description: "a plate!",
-    url: "https://www.tunturi.org/website/Accessoires/14TUSCL381.jpg"
+    url: "./assets/barbell.jpg"
   },
   {
     id: "5",
     title: "Pull-Up Bar",
     description: "a pull bar!",
-    url: "https://www.repfitness.com/media/catalog/product/w/e/web-381b0466_dims.jpg"
+    url: "./assets/pull.jpg"
   },
   {
     id: "6",
     title: "Kettlebell",
     description: "a kettle!",
-    url: "https://cdn.webshopapp.com/shops/281654/files/289054299/pe-kettlebell.jpg"
+    url: "./assets/bell.jpg"
   },
   {
     id: "7",
     title: "Jumping Box",
     description: "a box!",
-    url: "http://t0.gstatic.com/images?q=tbn%3AANd9GcQT-rd4q4z6QmThrkCQNxKb8nzytowcZLemchvrW5iiJ-d0KXclkO0qqzx88gleZGMzsw4Y6MJw&usqp=CAc"
+    url: "./assets/box.jpeg"
   },
   {
     id: "8",
     title: "Rings",
     description: "a ring!",
-    url: "https://www.roguefitness.com/media/catalog/product/cache/1/rogue_header_2015/472321edac810f9b2465a359d8cdc0b5/r/o/rogue-wood-rings-header1.jpg"
+    url: "./assets/ring.webp"
   },
   {
     id: "9",
     title: "Resistance Band",
     description: "a band!",
-    url: "http://t0.gstatic.com/images?q=tbn%3AANd9GcSb9unY_w_KxAjAOYmgQpiVvs5ehKosInu6dKmu5O5rbJEGw0fNy60pCwP8HdDVHFNM2vZ_6e3c&usqp=CAc"
+    url: "./assets/band.jpeg"
   },
   {
     id: "10",
     title: "Sandbag",
     description: "a bag!",
-    url: "https://www.onnit.com/onnit/images/authority/6964.png?ver&w=735&auto=format"
+    url: "./assets/bag.png"
   },
   {
     id: "11",
     title: "None",
     description: "a body!",
-    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ4XxscgsT612bQY1TDCYRmnxu1NztlGbdZEvW1E_ck2Bbj_g3T"
+    url: "./assets/body.png"
   }
 ];
 
