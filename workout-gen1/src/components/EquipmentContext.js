12db3e723595529b8cbf284e88badcfbad338891
Ka Wong <kawong2020@u.northwestern.edu>
2020-02-10 19:32:04 -0600

    remove workout-gen1

--- workout-gen1/src/components/EquipmentContext.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-export const EquipmentContext = React.createContext({
-    lst: [],
-    updateLst: () => {}
-})
--- workout-gen1/src/components/utils.js
+++
@@ -1,5 +0,0 @@
-export const shuffleList = list =>
-  list
-    .map(a => [Math.random(), a])
-    .sort((a, b) => a[0] - b[0])
-    .map(a => a[1]);
