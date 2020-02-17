db4a446f040f2f3f433fa87ceabc5feefcd5ba28
Demitra Del Fiacco <ddelfiacco99@gmail.com>
2020-02-17 12:08:29 -0600

    setting up page linking

--- boardgames/src/App.js
+++ boardgames/src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useContext, useParams } from 'react';
 import HomePage from './Pages/Home/HomePage';
+import ListingPage from './Pages/Listing/ListingPage';
+import { AppState } from './context';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
+  const appState = useContext(AppState);
+  const { data } = appState;
+  const { id } = useParams();
 
   return (
     <BrowserRouter>
       <Route exact path="/" render={() => <HomePage />} />
+      <Route path="/:id" render={() => <ListingPage val={data[id]} /> } />
     </BrowserRouter>
   );
 }
