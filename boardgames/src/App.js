607707ce711de7424557f5e3bf941fdedd8b7b30
Demitra Del Fiacco <ddelfiacco99@gmail.com>
2020-02-17 16:37:36 -0600

    listing page updates

--- boardgames/src/App.js
+++ boardgames/src/App.js
@@ -1,19 +1,21 @@
-import React, { useContext, useParams } from 'react';
+import React, { useContext } from 'react';
 import HomePage from './Pages/Home/HomePage';
 import ListingPage from './Pages/Listing/ListingPage';
 import { AppState } from './context';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, useParams, useLocation } from 'react-router-dom';
+import Listing from './Pages/Listing/Listing1';
 
 function App() {
   const appState = useContext(AppState);
   const { data } = appState;
-  const { id } = useParams();
 
   return (
+    data.length !== 0 ? 
     <BrowserRouter>
       <Route exact path="/" render={() => <HomePage />} />
-      <Route path="/:id" render={() => <ListingPage val={data[id]} /> } />
+      <Route exact path="/:id" render={() => <ListingPage listing={data[0]} /> } />
     </BrowserRouter>
+    : null
   );
 }
 
--- boardgames/src/Pages/Listing/ListingPage.js
+++ boardgames/src/Pages/Listing/ListingPage.js
@@ -1,56 +1,33 @@
-import React, { useEffect, useState } from 'react';
-import { Label, Card, Rating, Icon, Feed, Segment, Divider, Image, Header, Button, Grid, Confirm } from "semantic-ui-react";
+import React from 'react';
+import { Grid, Image, Button, Header, Label, Container, Divider, Card } from 'semantic-ui-react';
 
-const Listing = (val) => {
-  let each = val["val"];
+const ListingPage = ({ listing }) => {
   return (
-    
-      <Card>
-        <Image src='./test.jpg' wrapped ui={false} />
-        <Card.Content>
-          <Card.Header>{each["Game"]}</Card.Header>
-          <Card.Meta>{each["Players"][0] + "-" + each["Players"][1] + " Players"}</Card.Meta>
-          <Card.Description>
-            {each["Description"]}
-          </Card.Description>
-        </Card.Content>
-        <Card.Content>
-          <h4> Tags </h4>
-          <div>
-            <Button compact size='mini'>Family friendly</Button>
-            <Button compact size='mini'>Funny</Button>
-
-          </div>
-        </Card.Content>
-        <Card.Content>
-          <Feed>
-            <h4> Owner </h4>
-            <Feed.Event>
-              <Feed.Label image='./test.jpg' />
-              <Feed.Content>
-                <Feed.Summary>
-                  John Smith<br />
-                  <Feed.Date content='johnsmith335' />
-                  <br />
-                  <Rating defaultRating={3} maxRating={5} disabled />
-                </Feed.Summary>
-              </Feed.Content>
-            </Feed.Event>
-          </Feed>
-        </Card.Content>
-        <Card.Content>
-          <div style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}>
-            <Button attached='left'>Contact User</Button>
-            <Button color='yellow' attached='right'>Request Rental</Button>
-          </div>
-        </Card.Content>
-      </Card>
-
+    <Container>
+      <Grid>
+        <Grid.Row>
+          <Image src={listing.images[0]} />
+        </Grid.Row>
+        <Grid.Row style={{ margin: "0px 10px 0px 10px "}}>
+          <Header>
+            {listing.game}
+            <Header.Subheader
+              content={`${listing.minPlayers}-${listing.maxPlayers} Players`}
+              style={{ fontStyle: "italic", color: "black"}}
+            />
+            <Header.Subheader content={listing.description} />
+          </Header>
+        </Grid.Row>
+        <Divider />
+        <Grid.Row style={{ margin: "0px 10px 0px 10px "}}>
+          <Header size="small">
+            Tags: 
+            {listing.genre.map(g => <Label content={g} />)}
+          </Header>
+        </Grid.Row>
+      </Grid>
+    </Container>
   );
-}
+};
 
-export default Listing;
+export default ListingPage;
