d7afd96f7019643537771deadbb943fcabba816b
Demitra Del Fiacco <ddelfiacco99@gmail.com>
2020-02-16 21:49:33 -0600

    reconfigured app page and removed unnecessary files

--- boardgames/src/App.js
+++ boardgames/src/App.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import HomePage from './Pages/HomePage';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { db } from './firebase';
 
@@ -15,22 +14,9 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Route exact path="/" render={() => <HomePage />} />
+    </BrowserRouter>
   );
 }
 
---
+++ boardgames/src/Pages/HomePage.js
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react';
+import { Grid, Header, Container, Card } from 'semantic-ui-react';
+
+const HomePage = () => {
+  return (
+    <Container>
+      <Header content="Marketplace" />
+    </Container>
+  );
+};
+
+export default HomePage;
